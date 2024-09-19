0d8797dae9ba2e95a1cc192c37da15418d5b7c13
PanUcitelCz <104927244+PanUcitelCz@users.noreply.github.com>
2024-09-19 22:27:54 +0200

    asdf

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 import { SECRET_RESEND_API_KEY } from '$env/static/private';
-import { BASE_URL } from '$env/static/private';
+//import { BASE_URL } from '$env/static/private';
 
 export async function sendVerificationEmail(to: string, nickname: string, token: string) {
     const subject = "Verify Your Email";
-    const verifyLink = `${BASE_URL}/verify?token=${token}`;  // Dynamický link založený na BASE_URL
+    const verifyLink = `https://xp-life.cz/verify?token=${token}`;  // Dynamický link založený na BASE_URL
     const htmlContent = `Hello ${nickname},<br>Please verify your email by clicking on the following link: <a href="${verifyLink}">Verify Email</a>`;
 
     const response = await fetch('https://api.resend.com/emails', {
